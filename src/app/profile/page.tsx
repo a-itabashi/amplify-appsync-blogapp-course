1e5e1d6cef2d2a2dfbc570c56476f871c08ae06e
Aguru Itabashi <itabashi.a@gmail.com>
2024-03-26 13:53:41 +0900

    リファクタ

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,16 +1,19 @@
 "use client";
-import { withAuthenticator } from "@aws-amplify/ui-react";
+import { withAuthenticator, Button } from "@aws-amplify/ui-react";
 import {
   AuthUser,
   FetchUserAttributesOutput,
   fetchUserAttributes,
   getCurrentUser,
-  signOut,
 } from "aws-amplify/auth";
 import { useState, useEffect } from "react";
 import "@aws-amplify/ui-react/styles.css";
 
-function Profile() {
+type Props = {
+  signOut: () => void;
+};
+
+function Profile({ signOut }: Props) {
   const [authUser, setAuthUser] = useState<AuthUser | null>(null);
   const [userAttributes, setUserAttributes] =
     useState<FetchUserAttributesOutput | null>(null);
@@ -42,7 +45,7 @@ function Profile() {
       <h1 className="text-3xl font-semibold tracking-wide mt-6">Profile</h1>
       <h1 className="font-medium text-gray-500 my-2">{authUser.username}</h1>
       <p className="text-sm text-gray-500 mb-6">{userAttributes.email}</p>
-      <button onClick={() => signOut()}>Sign out</button>
+      <Button onClick={signOut}>Sign out</Button>
     </div>
   );
 }
