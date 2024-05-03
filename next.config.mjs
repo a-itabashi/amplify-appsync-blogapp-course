c22cbc5cf323c377323ed0c51f108f87c4312b2c
Aguru Itabashi <itabashi.a@gmail.com>
2024-05-03 16:43:51 +0900

    仮デプロイ完了

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
   images: {
     domains: [
       "amplifyappsyncblog006d327ee3c74303a67a79f6b78b0161645-dev.s3.ap-northeast-1.amazonaws.com",
+      "amplifyappsyncblog006d327ee3c74303a67a79f6b78b0a5d2e-production.s3.ap-northeast-1.amazonaws.com",
     ],
   },
 };
